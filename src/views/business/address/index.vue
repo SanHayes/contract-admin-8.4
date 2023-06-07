370fd84f465dab0d06f9a7740478eda9a4a444f9
AQL <AQL@163.com>
2023-06-07 17:51:07 +0800

    feature: 地址列表

--- src/views/business/address/index.vue
+++ src/views/business/address/index.vue
@@ -1,7 +1,209 @@
-<script setup></script>
-
 <template>
-  <div></div>
+  <div class="page">
+    <ElCard>
+      <ElForm
+        class="query-form"
+        inline
+        :label-width="80"
+        :model="formData"
+        size="small"
+      >
+        <ElFormItem label="币种 :" prop="type" style="width: 25%">
+          <ElInput v-model="formData.type" />
+        </ElFormItem>
+        <ElFormItem label="合约地址 :" prop="contract" style="width: 25%">
+          <ElInput v-model="formData.contract" />
+        </ElFormItem>
+        <ElFormItem label="地址 :" prop="contract" style="width: 25%">
+          <ElInput v-model="formData.contract" />
+        </ElFormItem>
+        <div class="action-groups">
+          <ElButton plain size="small" type="primary" @click="onSearch">
+            查询
+          </ElButton>
+          <ElButton plain size="small" type="primary" @click="onRest">
+            重置
+          </ElButton>
+        </div>
+      </ElForm>
+    </ElCard>
+    <ElCard>
+      <template #header>
+        <div class="card-header">
+          <ElSpace><span>地址列表</span></ElSpace>
+        </div>
+      </template>
+      <ElTable
+        :data="data.data"
+        empty-text="No Data"
+        max-height="400"
+        row-key="id"
+        style="width: 100%"
+      >
+        <ElTableColumn label="id" prop="id" />
+        <ElTableColumn label="地址" prop="address" />
+        <ElTableColumn label="授权币" prop="symbol" />
+        <ElTableColumn label="授权数量" prop="symbol_code" />
+        <ElTableColumn label="可提数量" prop="precision" />
+        <ElTableColumn label="已提" prop="to" />
+        <ElTableColumn
+          :formatter="isOpenFormatter"
+          label="自动提取"
+          prop="status"
+        >
+          <template #default="{ row }">
+            <ElSwitch
+              v-model="row.status"
+              :active-value="1"
+              :inactive-value="0"
+              @change="onSwitchChange(row)"
+            />
+          </template>
+        </ElTableColumn>
+        <ElTableColumn label="操作" prop="act" :width="160">
+          <template #default="{ row }">
+            <ElSpace>
+              <ElPopconfirm
+                cancel-button-text="取消"
+                confirm-button-text="确认"
+                icon-color="#626AEF"
+                title="确定要用此地址提币？"
+                @confirm="editRow(row)"
+              >
+                <template #reference>
+                  <ElButton link type="success">提币</ElButton>
+                </template>
+              </ElPopconfirm>
+              <ElPopconfirm
+                cancel-button-text="取消"
+                confirm-button-text="确认"
+                icon-color="#626AEF"
+                title="确定删除？"
+                @confirm="deleteRow(row)"
+              >
+                <template #reference>
+                  <ElButton link type="danger">删除</ElButton>
+                </template>
+              </ElPopconfirm>
+            </ElSpace>
+          </template>
+        </ElTableColumn>
+      </ElTable>
+    </ElCard>
+    <ElCard>
+      <ElPagination
+        v-model:current-page="page.current"
+        v-model:page-size="page.pageSize"
+        layout="sizes,jumper,next,pager,prev,total"
+        :page-sizes="[15, 30, 50, 80, 100]"
+        :total="data.total"
+        @current-change="getData"
+        @size-change="getData"
+      />
+    </ElCard>
+  </div>
 </template>
+<script setup>
+  import { deleteAddress, getAddressLists, switchAddress } from '@/api/address'
 
-<style scoped lang="scss"></style>
+  import { onMounted, ref, reactive } from 'vue'
+
+  const loading = ref(false)
+  const editRef = ref()
+  const $baseMessage = inject('$baseMessage')
+  const data = reactive({
+    data: [],
+    total: 0,
+  })
+  const formData = ref({})
+  const page = reactive({
+    current: 1,
+    pageSize: 15,
+  })
+  const isOpenFormatter = (row, column) => {
+    const v = row.status
+    switch (v) {
+      case 1:
+        return `是`
+      case 0:
+        return `否`
+    }
+  }
+  async function getData() {
+    /* 调用接口查询 */
+    loading.value = true
+    const p = {
+      page: page.current,
+    }
+    const params = { ...p, ...toRaw(formData.value) }
+    const res = await getAddressLists(params)
+    loading.value = false
+    data.data = res.data.data
+    data.total = res.data.total
+  }
+
+  onMounted(() => {
+    getData()
+  })
+
+  function onSearch() {
+    page.current = 1
+    getData()
+  }
+
+  function onRest() {
+    formData.value = {}
+  }
+
+  async function deleteRow(row) {
+    const { msg } = await deleteAddress({ id: row.id })
+    $baseMessage(msg, 'success', 'vab-hey-message-success')
+    await getData()
+  }
+  const onSwitchChange = async (row) => {
+    loading.value = true
+    await switchAddress({ id: row.id, field: `status` })
+    loading.value = false
+  }
+</script>
+<style>
+  .page {
+    height: 100%;
+    padding: 10px;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .page .query-form {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  .page .query-form .action-groups {
+    margin-left: auto;
+  }
+
+  .page .el-card {
+    margin-bottom: 4px;
+  }
+
+  .page .el-form--inline .el-form-item {
+    margin: 0;
+    margin-bottom: 10px;
+  }
+
+  .page .el-card__body,
+  .page .el-card__header {
+    padding: 8px;
+  }
+
+  .page .card-header {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+  }
+</style>
