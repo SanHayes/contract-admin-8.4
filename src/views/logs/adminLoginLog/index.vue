b03ed3cc10568d061db3c56e508fd559c566d38c
AQL <AQL@163.com>
2023-06-22 01:05:36 +0800

    feature: admin_login_log

--- src/views/logs/adminLoginLog/index.vue
+++ src/views/logs/adminLoginLog/index.vue
@@ -1,151 +1,137 @@
 <template>
-  <div class="system-log-container">
-    <vab-query-form>
-      <vab-query-form-top-panel>
-        <el-form
-          inline
-          label-width="60px"
-          :model="state.queryForm"
-          @submit.prevent
-        >
-          <el-form-item label="账号">
-            <el-input
-              v-model.trim="state.queryForm.account"
-              clearable
-              placeholder="请输入账号"
-            />
-          </el-form-item>
-          <el-form-item label="周期">
-            <el-date-picker
-              v-model="state.queryForm.searchDate"
-              end-placeholder="结束日期"
-              start-placeholder="开始日期"
-              type="daterange"
-            />
-          </el-form-item>
-          <el-form-item>
-            <el-button :icon="Search" type="primary" @click="queryData">
-              查询
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </vab-query-form-top-panel>
-    </vab-query-form>
-
-    <el-table v-loading="state.listLoading" :data="state.list">
-      <el-table-column type="expand">
-        <template #default="{ row }">
-          <div class="vab-table-expand">
-            <p>
-              <span class="vab-table-expand-title">日志类型:</span>
-              {{ row.type }}
-            </p>
-            <p>
-              <span class="vab-table-expand-title">账号:</span>
-              {{ row.account }}
-            </p>
-            <p>
-              <span class="vab-table-expand-title">执行结果:</span>
-              <span v-if="row.executeResult === '登录成功'">
-                <span class="vab-dot vab-dot-success"><span></span></span>
-                {{ row.executeResult }}
-              </span>
-              <span v-else>
-                <span class="vab-dot vab-dot-error"><span></span></span>
-                {{ row.executeResult }}
-              </span>
-            </p>
-            <p>
-              <span class="vab-table-expand-title">登录IP:</span>
-              {{ row.ip }}
-            </p>
-            <p>
-              <span class="vab-table-expand-title">访问时间:</span>
-              {{ row.datetime }}
-            </p>
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="日志类型"
-        prop="type"
-        show-overflow-tooltip
-        width="230px"
-      />
-      <el-table-column label="账号" prop="account" show-overflow-tooltip />
-      <el-table-column
-        label="执行结果"
-        prop="executeResult"
-        show-overflow-tooltip
+  <div class="page">
+    <ElCard>
+      <ElForm
+        class="query-form"
+        inline
+        :label-width="80"
+        :model="formData"
+        size="small"
       >
-        <template #default="{ row }">
-          <span v-if="row.executeResult === '登录成功'">
-            <span class="vab-dot vab-dot-success"><span></span></span>
-            {{ row.executeResult }}
-          </span>
-          <span v-else>
-            <span class="vab-dot vab-dot-error"><span></span></span>
-            {{ row.executeResult }}
-          </span>
-        </template>
-      </el-table-column>
-      <el-table-column label="登录IP" prop="ip" />
-      <el-table-column label="访问时间" prop="datetime" show-overflow-tooltip />
-      <template #empty>
-        <el-empty class="vab-data-empty" description="暂无数据" />
+        <ElFormItem label="登录IP :" prop="ip" style="width: 25%">
+          <ElInput v-model="formData.ip" />
+        </ElFormItem>
+        <ElFormItem label="请求域名 :" prop="domain" style="width: 25%">
+          <ElInput v-model="formData.domain" />
+        </ElFormItem>
+        <ElFormItem label="用户名 :" prop="admin_id" style="width: 25%">
+          <ElInput v-model="formData.admin_id" />
+        </ElFormItem>
+        <div class="action-groups">
+          <ElButton plain size="small" type="primary" @click="onSearch">
+            查询
+          </ElButton>
+          <ElButton plain size="small" type="primary" @click="onRest">
+            重置
+          </ElButton>
+        </div>
+      </ElForm>
+    </ElCard>
+    <ElCard>
+      <template #header>
+        <div class="card-header">
+          <ElSpace><span>登录日志</span></ElSpace>
+          <ElSpace />
+        </div>
       </template>
-    </el-table>
-    <el-pagination
-      background
-      :current-page="state.queryForm.pageNo"
-      :layout="state.layout"
-      :page-size="state.queryForm.pageSize"
-      :total="state.total"
-      @current-change="handleCurrentChange"
-      @size-change="handleSizeChange"
-    />
+      <ElTable
+        :data="data.data"
+        empty-text="No Data"
+        max-height="400"
+        row-key="id"
+        style="width: 100%"
+      >
+        <ElTableColumn label="id" prop="id" />
+        <ElTableColumn label="登录IP" prop="ip" />
+        <ElTableColumn label="请求域名" prop="domain" />
+        <ElTableColumn label="用户名" prop="admin_id" />
+        <ElTableColumn label="内容" prop="content" />
+        <ElTableColumn label="登录时间" prop="create_time" />
+      </ElTable>
+    </ElCard>
+    <ElCard>
+      <ElPagination
+        v-model:current-page="page.current"
+        v-model:page-size="page.pageSize"
+        layout="sizes,jumper,next,pager,prev,total"
+        :page-sizes="[15, 30, 50, 80, 100]"
+        :total="data.total"
+        @current-change="getData"
+        @size-change="getData"
+      />
+    </ElCard>
   </div>
 </template>
-
 <script setup>
-  import { getList } from '@/api/systemLog'
-  import { Search } from '@element-plus/icons-vue'
-
-  const state = reactive({
-    list: [],
-    listLoading: true,
-    layout: 'total, sizes, prev, pager, next, jumper',
-    total: 0,
-    queryForm: {
-      account: '',
-      searchDate: '',
-      pageNo: 1,
-      pageSize: 20,
-    },
+  import { mock } from 'mockjs'
+  import { onMounted, ref, reactive } from 'vue'
+  const data = reactive({
+    data: [],
+    total: 100,
   })
-
-  const fetchData = async () => {
-    state.listLoading = true
-    const {
-      data: { list, total },
-    } = await getList(state.queryForm)
-    state.list = list
-    state.total = total
-    state.listLoading = false
-  }
-  const handleSizeChange = (val) => {
-    state.queryForm.pageSize = val
-    fetchData()
-  }
-  const handleCurrentChange = (val) => {
-    state.queryForm.pageNo = val
-    fetchData()
-  }
-  const queryData = () => {
-    state.queryForm.pageNo = 1
-    fetchData()
+  const formData = ref({})
+  const page = reactive({
+    current: 1,
+    pageSize: 15,
+  })
+  async function getData() {
+    /* 调用接口查询 */
+    const rowData = await Array.from({ length: page.pageSize }).map((_) =>
+      mock({
+        id: '@id',
+        ip: '@ip',
+        domain: '@url',
+        admin_id: '@cname',
+        content: '@cword()',
+        create_time: '@date',
+      })
+    )
+    data.data = rowData
+    data.total = mock('@integer(200, 300)')
   }
   onMounted(() => {
-    fetchData()
+    getData()
   })
+  function onSearch() {
+    page.current = 1
+    getData()
+  }
+  function onRest() {
+    formData.value = {}
+  }
 </script>
+<style>
+  .page {
+    height: 100%;
+    padding: 10px;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  .page .query-form {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  .page .query-form .action-groups {
+    margin-left: auto;
+  }
+  .page .el-card {
+    margin-bottom: 4px;
+  }
+  .page .el-form--inline .el-form-item {
+    margin: 0;
+    margin-bottom: 10px;
+  }
+  .page .el-card__body,
+  .page .el-card__header {
+    padding: 8px;
+  }
+  .page .card-header {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+  }
+</style>
