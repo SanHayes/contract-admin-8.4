ea678c5b4a6bdf0b8c8a7c53ba95081829030400
AQL <AQL@163.com>
2023-06-22 16:57:59 +0800

    feature: 财务相关api

---
+++ src/api/finance.ts
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+
+/**
+ * 用户提现记录
+ * @param data
+ */
+export function getUserExchangeList(data: any = {}) {
+  return request({
+    url: 'admin/finance/userExchange',
+    method: 'post',
+    data,
+  })
+}
+
+/**
+ * 审核
+ * @param data
+ */
+export function statusUserExchange(data: any = {}) {
+  return request({
+    url: 'admin/finance/userStatus',
+    method: 'post',
+    data,
+  })
+}
+
