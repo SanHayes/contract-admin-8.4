80adcccc5d361a99b0748684cb84595c9824dc29
l_usher <moaom@proton.me>
2023-06-24 00:31:45 +0800

    update 白皮书设置

---
+++ src/views/system/whitepaper/components/Edit.vue
@@ -0,0 +1,186 @@
+<template>
+  <ElDialog
+    v-model="state.dialogFormVisible"
+    :title="state.name"
+    width="850px"
+    @close="close"
+  >
+    <ElForm ref="formRef" :model="state.form" :rules="rules" label-width="150px">
+      <ElFormItem label="标题" prop="title">
+        <ElInput v-model="state.form.title" placeholder="请填写标题" />
+      </ElFormItem>
+      <ElFormItem label="上传图片" prop="icon">
+        <el-upload
+          :action="state.uploadUrl"
+          style="width: 250px"
+          :before-upload="beforeUploadUpload"
+          drag
+          class="upload-demo"
+          :headers="state.uploadHeaders"
+          :on-success="uploadImg"
+          :show-file-list="false"
+        >
+          <ElImage
+            v-if="state.form.icon"
+            fit="contain"
+            :src="state.form.icon"
+            style="width: 100px; height: 100px"
+          />
+          <el-icon class="el-icon--upload" v-else><upload-filled /></el-icon>
+        </el-upload>
+      </ElFormItem>
+      <ElFormItem label="文件" prop="file">
+        <ElUpload
+          :action="state.uploadUrl"
+          :before-upload="beforeUploadUpload"
+          class="avatar-uploader"
+          :headers="state.uploadHeaders"
+          :on-success="handleLogoUploadSuccess"
+          :show-file-list="false"
+        >
+          <ElImage
+            v-if="state.form.file"
+            fit="contain"
+            :src="state.form.file"
+            style="width: 100px; height: 100px"
+          />
+          <ElButton v-else type="primary">点击上传</ElButton>
+        </ElUpload>
+      </ElFormItem>
+
+      <ElFormItem class="mg_t_20">
+        <ElButton type="plain" @click="close">取消</ElButton>
+        <ElButton type="primary" @click="onSubmit">提交</ElButton>
+      </ElFormItem>
+    </ElForm>
+  </ElDialog>
+</template>
+
+<script setup>
+import { saveWhitePaper } from "@/api/paper";
+import { useUserStore } from "@/store/modules/user";
+import StartEvent from "@logicflow/extension/es/bpmn/events/StartEvent";
+import { ref, reactive } from "vue";
+
+const userStore = useUserStore();
+const { token } = userStore;
+
+const state = reactive({
+  dialogFormVisible: false,
+  uploadUrl: `${process.env.VUE_APP_BASE_URL}/admin/Upload/index`,
+  uploadHeaders: {
+    Authorization: `Bearer ${token}`,
+  },
+  form: {},
+  confirm_password: "",
+});
+const language = ref("en");
+
+//   表单部分
+const formRef = ref();
+const rules = reactive({
+  icon: [{ required: true, message: "请上传图片", trigger: "blur" }],
+  file: [{ required: true, message: "请上传文件", trigger: "blur" }],
+  title: [{ required: true, message: "标题不能为空", trigger: "blur" }],
+});
+// @todo 集中管理
+const langs = {
+  "zh-TW": "繁体中文",
+  en: "英文",
+  ja: "日语",
+  es: "西班牙语",
+  vi: "越南语",
+  IN: "印尼语",
+  hi: "印地语",
+};
+// const langFormatter = (row, column) => {
+//   const v = row.language
+//   return langs[v] ?? ''
+// }
+
+const $baseMessage = inject("$baseMessage");
+
+const emits = defineEmits(["fetch-data"]);
+
+// 上传文件/图片
+const beforeUploadUpload = (rawFile) => {
+  console.log(`beforeUploadUpload`, rawFile);
+};
+const handleLogoUploadSuccess = (response, uploadFile) => {
+  const { code, data } = response;
+  if (code === 200) {
+    state.form.file = data.path;
+  }
+};
+// 成功上传图片
+const uploadImg = (response, uploadFile) => {
+  const { code, data } = response;
+  if (code === 200) {
+    state.form.icon = data.path;
+  }
+};
+
+const close = () => {
+  formRef.value.resetFields();
+  state.form = {};
+  state.dialogFormVisible = false;
+};
+
+const showEdit = (row) => {
+  if (!row) {
+    state.name = "添加";
+  } else {
+    state.name = "编辑";
+    state.form = JSON.parse(JSON.stringify(row));
+  }
+  state.dialogFormVisible = true;
+};
+
+const onSubmit = () => {
+  formRef.value.validate(async (valid) => {
+    if (valid) {
+      state.form.language = state.form.language || language.value;
+      const { code, msg } = await saveWhitePaper(state.form);
+      if (code === 200) {
+        $baseMessage(msg, "success", "vab-hey-message-success");
+      } else {
+        $baseMessage(msg, "error", "vab-hey-message-error");
+      }
+      emits("fetch-data");
+      close();
+    }
+  });
+};
+
+defineExpose({
+  showEdit,
+});
+</script>
+
+<style scoped lang="scss">
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
--- src/views/system/whitepaper/index.vue
+++ src/views/system/whitepaper/index.vue
@@ -1,7 +1,146 @@
-<script setup></script>
-
 <template>
-  <div></div>
+  <div class="page">
+    <ElCard>
+      <template #header>
+        <div class="card-header">
+          <ElSpace><span>白皮书设置</span></ElSpace>
+          <ElSpace> </ElSpace>
+          <ElSpace>
+            <ElButton class="button" plain type="primary" @click="editRow($event)">
+              新增
+            </ElButton>
+          </ElSpace>
+        </div>
+      </template>
+      <ElTable
+        :data="data.data"
+        style="width: 100%"
+        max-height="400"
+        row-key="id"
+        empty-text="No Data"
+      >
+        <ElTableColumn prop="icon" label="图片">
+          <template #default="{ row }">
+            <ElImage fit="contain" :src="row.icon" style="width: 100px; height: 100px" />
+          </template>
+        </ElTableColumn>
+        <ElTableColumn prop="title" label="标题" />
+        <ElTableColumn prop="file" label="文件链接" />
+        <ElTableColumn prop="act" label="操作" :width="160">
+          <template #default="{ row }">
+            <ElSpace>
+              <ElButton link type="success" @click="editRow(row)"> 编辑 </ElButton>
+              <ElPopconfirm
+                cancel-button-text="取消"
+                confirm-button-text="确认"
+                icon-color="#626AEF"
+                title="确定删除？"
+                @confirm="deleteRow(row)"
+              >
+                <template #reference>
+                  <ElButton link type="danger">删除</ElButton>
+                </template>
+              </ElPopconfirm>
+            </ElSpace>
+          </template>
+        </ElTableColumn>
+      </ElTable>
+    </ElCard>
+    <ElCard>
+      <ElPagination
+        layout="sizes,jumper,next,pager,prev,total"
+        :total="data.total"
+        :page-sizes="[15, 30, 50, 80, 100]"
+        v-model:page-size="page.pageSize"
+        v-model:current-page="page.current"
+        @current-change="getData"
+        @size-change="getData"
+      />
+    </ElCard>
+    <Edit ref="editRef" @fetch-data="getData" />
+  </div>
 </template>
+<script setup>
+import { onMounted, ref, reactive } from "vue";
+import Edit from "./components/Edit.vue";
+import { getWhitePaperList, deleteWhitePaper } from "@/api/paper";
 
-<style scoped lang="scss"></style>
+const loading = ref(false);
+const editRef = ref();
+const $baseMessage = inject("$baseMessage");
+
+const data = reactive({
+  data: [],
+  total: 0,
+});
+
+// 新增 / 编辑
+const editRow = (row) => {
+  console.log(`editRow`, row);
+  if (row.id) {
+    editRef.value.showEdit(row);
+  } else {
+    editRef.value.showEdit();
+  }
+};
+
+const page = reactive({
+  current: 1,
+  pageSize: 15,
+});
+async function getData() {
+  /* 调用接口查询 */
+  loading.value = true;
+  const res = await getWhitePaperList(toRaw(data));
+  loading.value = false;
+  data.data = res.data.data;
+  data.total = res.data.total;
+}
+
+// 删除
+async function deleteRow(row) {
+  const { msg } = await deleteWhitePaper({ id: row.id });
+  $baseMessage(msg, "success", "vab-hey-message-success");
+  await getData();
+}
+
+onMounted(() => {
+  getData();
+});
+</script>
+
+<style>
+.page {
+  height: 100%;
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.1);
+}
+.page .query-form {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+}
+.page .query-form .action-groups {
+  margin-left: auto;
+}
+.page .el-card {
+  margin-bottom: 4px;
+}
+.page .el-form--inline .el-form-item {
+  margin: 0;
+  margin-bottom: 10px;
+}
+.page .el-card__body,
+.page .el-card__header {
+  padding: 8px;
+}
+.page .card-header {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
