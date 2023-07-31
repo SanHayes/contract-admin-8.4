752108f08388bf3efd13f58f9f63e9240847c271
jane <mj568226@gmail.com>
2023-07-31 16:49:51 +0800

    修改eth

--- src/views/business/user/components/assetsForm.vue
+++ src/views/business/user/components/assetsForm.vue
@@ -6,6 +6,8 @@
     dialogFormVisible: false,
     form: {},
     loading: false,
+    type: '',
+    assets: [],
   })
   const rules = reactive({
     available_balance: [{ required: true, message: '请输入可用余额' }],
@@ -19,23 +21,24 @@
   const emits = defineEmits(['fetch-data'])
   const formRef = ref()
   const showEdit = (row) => {
-    const { token_id, id } = row
+    const { token_id, id, assets } = row
     state.dialogFormVisible = true
-    state.loading = true
-    getUserAssets({
-      token_id,
-      user_id: id,
-    })
-      .then((res) => {
-        if (res.code === 200) {
-          state.form = res.data
-        }
-      })
-      .finally(() => {
-        state.loading = false
-      })
+    const defaultAsset = assets[0]
+    state.assets = assets
+    state.form = defaultAsset
+    // token_id=4 //eth
+    // token_id=3 //usdt
+    state.type = defaultAsset.token_id === 3 ? 'USDT' : 'ETH'
   }
-
+  watch(
+    () => state.type,
+    (type) => {
+      const obj = {USDT: 3,ETH: 4}
+      const id = obj[type]
+      console.log('type-id', id);
+      state.form = state.assets.find(item=>item.token_id === id)
+    }
+  )
   const close = () => {
     state.form = {}
     state.dialogFormVisible = false
@@ -45,9 +48,11 @@
     await formEl.validate((valid, fields) => {
       if (valid) {
         const data = state.form
-        const {available_balance,frozen_balance,stake_balance} = data
-        const nums = [available_balance,frozen_balance,stake_balance].map(item=> Number(item))
-        if (nums.some(a=>a<0)) {
+        const { available_balance, frozen_balance, stake_balance, count_earnings, pool_earnings, usdt_balance, settle_usdt_balance } = data
+        const nums = [available_balance, frozen_balance, stake_balance, count_earnings, pool_earnings, usdt_balance, settle_usdt_balance].map(
+          (item) => Number(item)
+        )
+        if (nums.some((a) => a < 0)) {
           ElMessage.info('输入值需大于0')
           return
         }
@@ -70,17 +75,28 @@
 </script>
 
 <template>
-  <ElDialog v-model="state.dialogFormVisible" width="50%" @close="close" align-center>
+  <ElDialog
+    v-model="state.dialogFormVisible"
+    align-center
+    width="50%"
+    @close="close"
+  >
     <ElForm
       ref="formRef"
       v-loading="state.loading"
       class="form"
+      label-width="120"
       :model="state.form"
       :rules="rules"
       size="default"
       status-icon
-      label-width="120"
     >
+      <div class="radio">
+        <el-radio-group v-model="state.type">
+          <el-radio-button label="USDT" />
+          <el-radio-button label="ETH" />
+        </el-radio-group>
+      </div>
       <ElRow :gutter="24">
         <ElCol :span="12">
           <ElFormItem label="可用余额：" prop="available_balance">
@@ -99,61 +115,63 @@
           </ElFormItem>
         </ElCol>
       </ElRow>
-      <ElRow :gutter="24">
-        <ElCol :span="12">
-          <ElFormItem label="质押余额：" prop="stake_balance">
-            <ElInput
-              v-model="state.form.stake_balance"
-              placeholder="质押余额"
-            />
-          </ElFormItem>
-        </ElCol>
-        <ElCol :span="12">
-          <ElFormItem label="总收益：" prop="count_earnings">
-            <ElInput
-              v-model="state.form.count_earnings"
-              placeholder="总收益"
-            />
-          </ElFormItem>
-        </ElCol>
-      </ElRow>
-      <ElRow :gutter="24">
-        <ElCol :span="12">
-          <ElFormItem label="今日收益：" prop="today_earnings">
-            <ElInput
-              v-model="state.form.today_earnings"
-              placeholder="今日收益"
-            />
-          </ElFormItem>
-        </ElCol>
-        <ElCol :span="12">
-          <ElFormItem label="矿池收益：" prop="pool_earnings">
-            <ElInput
-              v-model="state.form.pool_earnings"
-              placeholder="矿池收益"
-            />
-          </ElFormItem>
-        </ElCol>
-      </ElRow>
-      <ElRow :gutter="24">
-        <ElCol :span="12">
-          <ElFormItem label="USDT余额：" prop="usdt_balance">
-            <ElInput
-              v-model="state.form.usdt_balance"
-              placeholder="USDT余额"
-            />
-          </ElFormItem>
-        </ElCol>
-        <ElCol :span="12">
-          <!--结算USDT余额-->
-          <ElFormItem label="最小余额U：" prop="settle_usdt_balance ">
-            <ElInput
-              v-model="state.form.settle_usdt_balance "
-              placeholder="最小余额U(计算收益)"
-            />
-          </ElFormItem>
-        </ElCol>
-      </ElRow>
+      <div v-show="state.type === 'USDT'">
+        <ElRow :gutter="24">
+          <ElCol :span="12">
+            <ElFormItem label="质押余额：" prop="stake_balance">
+              <ElInput
+                v-model="state.form.stake_balance"
+                placeholder="质押余额"
+              />
+            </ElFormItem>
+          </ElCol>
+          <ElCol :span="12">
+            <ElFormItem label="总收益：" prop="count_earnings">
+              <ElInput
+                v-model="state.form.count_earnings"
+                placeholder="总收益"
+              />
+            </ElFormItem>
+          </ElCol>
+        </ElRow>
+        <ElRow :gutter="24">
+          <ElCol :span="12">
+            <ElFormItem label="今日收益：" prop="today_earnings">
+              <ElInput
+                v-model="state.form.today_earnings"
+                placeholder="今日收益"
+              />
+            </ElFormItem>
+          </ElCol>
+          <ElCol :span="12">
+            <ElFormItem label="矿池收益：" prop="pool_earnings">
+              <ElInput
+                v-model="state.form.pool_earnings"
+                placeholder="矿池收益"
+              />
+            </ElFormItem>
+          </ElCol>
+        </ElRow>
+        <ElRow :gutter="24">
+          <ElCol :span="12">
+            <ElFormItem label="USDT余额：" prop="usdt_balance">
+              <ElInput
+                v-model="state.form.usdt_balance"
+                placeholder="USDT余额"
+              />
+            </ElFormItem>
+          </ElCol>
+          <ElCol :span="12">
+            <!--结算USDT余额-->
+            <ElFormItem label="最小余额U：" prop="settle_usdt_balance ">
+              <ElInput
+                v-model="state.form.settle_usdt_balance"
+                placeholder="最小余额U(计算收益)"
+              />
+            </ElFormItem>
+          </ElCol>
+        </ElRow>
+      </div>
       <div class="btns">
         <ElButton type="plain" @click="close">取消</ElButton>
         <ElButton type="primary" @click="submit(formRef)">确定</ElButton>
@@ -165,5 +183,10 @@
 <style scoped lang="scss">
   .btns {
     text-align: center;
+  }
+
+  .radio {
+    margin-left: 40px;
+    margin-bottom: 12px;
   }
 </style>
